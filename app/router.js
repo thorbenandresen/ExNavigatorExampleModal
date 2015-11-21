d1cce4ce450194568a04093e2bcbf96e5d24e5a2
Thorben Andresen <thorben.andresen@gmail.com>
2015-11-20 19:23:28 -0500

    clean up

--- app/router.js
+++ app/router.js
@@ -10,30 +10,6 @@ var {
 var Button = require('react-native-button');
 
 let YourRouter = {
-  // getHomeRoute() {
-  //   return {
-  //     // Return a React component class for the scene. It receives a prop
-  //     // called `navigator` that you can use to push on more routes.
-  //     getSceneClass() {
-  //       return require('./homeScene');
-  //     },
-  //
-  //     // When this scene receives focus, you can run some code. We're just
-  //     // proxying the `didfocus` event that Navigator emits, so refer to
-  //     // Navigator's source code for the semantics.
-  //     onDidFocus(event) {
-  //       console.log('Home Scene received focus.');
-  //     },
-  //
-  //     // Return a string to display in the title section of the navigation bar.
-  //     // This route's title is displayed next to the back button when you push
-  //     // a new route on top of this one.
-  //     getTitle() {
-  //       return 'Home';
-  //     },
-  //   };
-  // },
-
 
   getHomeRoute(openModal) {
     return {
@@ -58,7 +34,7 @@ let YourRouter = {
       // This route's title is displayed next to the back button when you push
       // a new route on top of this one.
       getTitle() {
-        return 'App';
+        return 'Home';
       },
     };
   },
